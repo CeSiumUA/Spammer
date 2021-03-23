0059a9c8f049453f2c0d8c831860ee2f5d8b070d
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-03-23 06:47:05 +0200

    Added chat load

--- TLSpammer/Program.cs
+++ TLSpammer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using TLSharp.Core;
 
 namespace TLSpammer
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1630560
+            //4cea02b478b137866d6b72c660cd9280
+            TelegramSpammer tlSpammer = new TelegramSpammer(1630560, "4cea02b478b137866d6b72c660cd9280", "+380983860743");
+            string codeRequestResult = await tlSpammer.InitTelegramAsync();
+            if (!string.IsNullOrEmpty(codeRequestResult))
+            {
+                Console.WriteLine("Отправлен код аутентификации, его необходимо ввести сюда:");
+                string authCode = Console.ReadLine();
+                var loginResult = await tlSpammer.LoginWithCodeAsync(authCode, codeRequestResult);
+                if (!loginResult)
+                {
+                    Console.WriteLine("Неправильный код, либо пользователь имеет Cloud Password! При наличии Cloud Password, введите его сюда:");
+                    var cloudPassword = Console.ReadLine();
+                    await tlSpammer.LoginWithPasswordAsync(cloudPassword);
+                }
+            }
+
+            await tlSpammer.StartSpammerAsync();
         }
     }
 }
---
+++ TLSpammer/TelegramSpammer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+
+namespace TLSpammer
+{
+    public class TelegramSpammer
+    {
+        private readonly TelegramClient telegramClient;
+        private readonly string login;
+        private TLUser user;
+        public TelegramSpammer(int appId, string appHash, string login)
+        {
+            this.login = login;
+            this.telegramClient = new TelegramClient(appId, appHash);
+        }
+
+        public async Task<string> InitTelegramAsync()
+        {
+            await telegramClient.ConnectAsync();
+            if (!telegramClient.IsUserAuthorized())
+            {
+                return await telegramClient.SendCodeRequestAsync(this.login);
+            }
+
+            return null;
+        }
+
+        public async Task StartSpammerAsync()
+        {
+            try
+            {
+                TLVector<TLAbsChat> chats = new TLVector<TLAbsChat>();
+                try
+                {
+                    var dialogs = (TLDialogsSlice)await telegramClient.GetUserDialogsAsync();
+                    chats = dialogs.Chats;
+                }
+                catch
+                {
+                    var dialogs = (TLDialogs)await telegramClient.GetUserDialogsAsync();
+                    chats = dialogs.Chats;
+                }
+                finally
+                {
+                    foreach (var chat in chats)
+                    {
+                        if (chat is TLChannel)
+                        {
+                            
+                        }
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+        public async Task<bool> LoginWithCodeAsync(string code, string hash)
+        {
+            try
+            {
+                this.user = await telegramClient.MakeAuthAsync(this.login, hash, code);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public async Task LoginWithPasswordAsync(string code)
+        {
+            this.user = await telegramClient.MakeAuthWithPasswordAsync(await telegramClient.GetPasswordSetting(), code);
+        }
+    }
+}
